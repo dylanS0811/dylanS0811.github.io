927b76aa57779d471ef0d344a5d28b3c0e262569
dylanS <dylansong0811@gmail.com>
2025-05-07 23:07:24 -0500

    1

--- src/lab02/Lab02App.jsx
+++ src/lab02/Lab02App.jsx
@@ -1,23 +1,23 @@
 // src/lab02/Lab02App.jsx
 import React from 'react';
-import './lab02.css';
+import styles from './lab02.module.css';
 
 export default function Lab02App() {
   return (
-    <div className="container">
-      <h1>Haining Song</h1>
-      <p>Just a programmer who enjoys playing piano and singing.</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Haining Song</h1>
+      <p className={styles.description}>Just a programmer who enjoys playing piano and singing.</p>
 
-      <h2>Current MIU Courses</h2>
-      <ul>
+      <h2 className={styles.sectionTitle}>Current MIU Courses</h2>
+      <ul className={styles.list}>
         <li>CS500 STC</li>
         <li>CS390 FPP</li>
         <li>CS401 MPP</li>
         <li>CS472 WAP</li>
       </ul>
 
-      <h2>My Favorite Movies and Series</h2>
-      <ol>
+      <h2 className={styles.sectionTitle}>My Favorite Movies and Series</h2>
+      <ol className={styles.list}>
         <li>
           <a href="https://www.imdb.com/title/tt0903747/" target="_blank" rel="noreferrer">
             Breaking Bad
@@ -27,24 +27,24 @@ export default function Lab02App() {
         <li>Black Mirror</li>
       </ol>
 
-      <h2>My Mood</h2>
-      <div className="mood-images">
-        <div className="mood-item">
+      <h2 className={styles.sectionTitle}>My Mood</h2>
+      <div className={styles.moodImages}>
+        <div className={styles.moodItem}>
           <img src="/images/happy.png" alt="Happy" />
-          <p className="mood-label happy">Happy</p>
+          <p className={`${styles.moodLabel} ${styles.happy}`}>Happy</p>
         </div>
-        <div className="mood-item">
+        <div className={styles.moodItem}>
           <img src="/images/sad.png" alt="Sad" />
-          <p className="mood-label sad">Sad</p>
+          <p className={`${styles.moodLabel} ${styles.sad}`}>Sad</p>
         </div>
       </div>
 
-      <h2>Fun Fact About My Neighbors</h2>
-      <p>
+      <h2 className={styles.sectionTitle}>Fun Fact About My Neighbors</h2>
+      <p className={styles.description}>
         <b>Furkan</b> is really good at the game :)
       </p>
 
-      <div className="validator-wrapper">
+      <div className={styles.validatorWrapper}>
         <a
           href="https://validator.w3.org/check?url=https://dylans0811.github.io/lab02/aboutme.html"
           target="_blank"
