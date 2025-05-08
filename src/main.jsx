ffcb3a82380e1a8aa8d1e243896b8c8aa359fa78
dylanS <dylansong0811@gmail.com>
2025-05-07 22:57:49 -0500

    Reconstruction

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './Homepage.jsx';
 import Lab09App from './lab09/App.jsx';
+import Lab02App from './lab02/Lab02App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
